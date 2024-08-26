c0890db24136222ef3ce9d86459b41cffc10da93
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-08-26 23:59:36 +0900

    1193

---
+++ Project1/1193.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+int main() {
+    ios_base::sync_with_stdio(false);
+    int num;
+    cin >> num;
+    int i = 1, j = 1;
+    for (int k = 1; k <= num; k++) {
+        
+    }
+    cout << i << "\/" << j;
+}
+*/
