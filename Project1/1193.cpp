5e441b49c259a25719063939186ce09b4ad768c6
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-08-28 23:52:29 +0900

    1193 풀이 완료

--- Project1/1193.cpp
+++ Project1/1193.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
-/*
+
 int main() {
     ios_base::sync_with_stdio(false);
     int num;
     cin >> num;
-    int i = 1, j = 1;
-    for (int k = 1; k <= num; k++) {
-        
+
+    int linenum = 0;
+    int sum = 0;
+
+    while (num > sum) {
+        linenum += 1;
+        sum += linenum;
     }
-    cout << i << "\/" << j;
+ 
+    int n = sum - num;
+    if (linenum % 2)
+        cout << 1 + n << "/" << linenum - n;
+    else
+        cout << linenum - n << "/" << 1 + n;
 }
-*/
