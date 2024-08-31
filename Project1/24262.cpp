2ba12b5d2ad70967c38b046d666e71dc13c1067a
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-08-31 23:09:48 +0900

    24262

---
+++ Project1/24262.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+MenOfPassion(A[], n) {
+	i = ⌊n / 2⌋;
+	return A[i]; # 코드1
+}
+
+n에 관계없이 항상 상수 시간 소요 
+
+*/
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	int n;
+
+	cin >> n;
+
+	cout << 1 << endl << 0;
+}
