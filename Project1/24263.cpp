22d0a9d72a92cd880842dd651bb29f31d851721a
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-08-31 23:14:12 +0900

    24263

---
+++ Project1/24263.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/*
+MenOfPassion(A[], n) {
+	sum <- 0;
+	for i <- 1 to n
+		sum <- sum + A[i]; # 코드1
+	return sum;
+}
+
+n에 비례하여 코드 수행
+*/
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	int n;
+
+	cin >> n;
+
+	cout << n << endl << 1;
+}
