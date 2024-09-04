391f0e4bd8f112c677c853b47fdb1efc9a786a58
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-09-04 22:18:12 +0900

    9063

---
+++ Project1/15894.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int num;
+    cin >> num;
+
+    long long sum;
+    sum = num * 4;
+    cout << sum;
+
+}
