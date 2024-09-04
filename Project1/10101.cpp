d9a06eeec59de8d52a7ceba278874ce7ea3c391e
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-09-05 00:05:52 +0900

    10101

---
+++ Project1/10101.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if ((a + b + c) != 180)
+        cout << "Error";
+    else if (a == b && b == c)
+        cout << "Equilateral";
+    else if (a != b && a != c && b != c)
+        cout << "Scalene";
+    else
+        cout << "Isosceles";
+}
