ef6bc79d4fc353075623e1809bb0bc44cde80c0a
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-09-04 22:55:20 +0900

    Update 15894.cpp

--- Project1/15894.cpp
+++ Project1/15894.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int main() {
@@ -8,25 +7,8 @@ int main() {
     int num;
     cin >> num;
 
-    int x, y;
-    int minX = 10000;
-    int minY = 10000;
-    int maxX = -10000;
-    int maxY = -10000;
-
-    for (int i = 0; i < num; i++) {
-        cin >> x;
-        cin >> y;
-        if (x < minX)
-            minX = x;
-        if (x > maxX)
-            maxX = x;
-        if (y < minY)
-            minY = y;
-        if (y > maxY)
-            maxY = y;
-    }
-
-    cout << (maxX - minX) * (maxY - minY);
+    unsigned int sum;
+    sum = num * 4;
+    cout << sum;
 
 }
