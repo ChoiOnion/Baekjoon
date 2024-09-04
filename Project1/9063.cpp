a36074e6c3103d56820d4b063a822f0e1f6bd73b
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-09-04 22:19:02 +0900

    Update 9063.cpp

---
+++ Project1/9063.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int num;
+    cin >> num;
+
+    unsigned int sum;
+    sum = num * 4;
+    cout << sum;
+
+}
