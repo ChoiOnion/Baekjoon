0461dd61208f03e76cb15c161a8f09739ab08afb
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-09-23 00:35:49 +0900

    1929

---
+++ Project1/1929.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+bool isPrime(int num)
+{
+    if (num <= 1)
+        return false;
+
+    if (num == 2 || num == 3)
+        return true;
+
+    if (num % 2 == 0 || num % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= num; ++i)
+    {
+        if (num % i == 0 || num % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int a, b;
+    cin >> a >> b;
+
+    while (a <= b) {
+        if (isPrime(a))
+            cout << a << endl;
+        a++;
+    }
+
+}
