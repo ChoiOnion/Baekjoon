803c4348d711778bd2aac4588a20c2f8d2bf8711
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-10-14 22:09:10 +0900

    10773

---
+++ Project1/10773.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    stack<int>st;
+
+    int k;
+
+    cin >> k;
+
+    for (int i = 0; i < k; i++) {
+        int n;
+        cin >> n;
+        if (n == 0)
+            st.pop();
+        else
+            st.push(n);
+    }
+    int sum = 0;
+
+    while (!st.empty()) {
+        sum += st.top();
+        st.pop();
+    }
+
+    cout << sum;
+}
