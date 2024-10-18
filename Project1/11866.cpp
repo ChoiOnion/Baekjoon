45043b15f5030a2a59a3e2da278a7c28acd6e4c7
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-10-19 00:40:39 +0900

    11866

---
+++ Project1/11866.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    queue<int> circle;
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+        circle.push(i);
+
+    cout << "<";
+    while (circle.size() != 0) {
+        int count = 1;
+        while (count != k) {
+            circle.push(circle.front());
+            circle.pop();
+            count++;
+        }
+        cout << circle.front();
+        if (circle.size() != 1)
+            cout << ", ";
+        circle.pop();
+    }
+    cout << ">";
+}
