93e4223205d93b53867901982ffd0ad2966bb35d
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-10-27 01:47:37 +0900

    1037

---
+++ Project1/1037.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    int arr[51];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr, arr + n);
+
+    cout << arr[0] * arr[n - 1];
+
+}
