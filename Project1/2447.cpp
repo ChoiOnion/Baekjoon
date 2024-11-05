0d5f09930e683826960a1f262f8add30809f9309
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-11-06 00:17:10 +0900

    2447

---
+++ Project1/2447.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+void star(int n, int i, int j) {
+    if ((i / n) % 3 == 1 && (j / n) % 3 == 1)
+        cout << " ";
+    else if (n / 3 == 0)
+        cout << "*";
+    else
+        star(n / 3, i, j);
+}
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            star(n, i, j);
+        }
+        cout << "\n";
+    }
+
+}
