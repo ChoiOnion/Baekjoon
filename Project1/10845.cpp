884be453290aab9105bc6792c07330a5875a9172
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-11-16 01:48:59 +0900

    10845

---
+++ Project1/10845.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+
+    for (int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+        if (str == "push") {
+            int temp;
+            cin >> temp;
+            q.push(temp);
+        }else if (str == "pop") {
+            if (q.empty())
+                cout << -1 << "\n";
+            else {
+                cout << q.front() << "\n";
+                q.pop();
+            }
+        }
+        else if (str == "size") {
+            cout << q.size() << "\n";
+        }
+        else if (str == "empty") {
+            if (q.empty())
+                cout << 1 << "\n";
+            else {
+                cout << 0 << "\n";
+            }
+        }
+        else if (str == "front") {
+            if (q.empty())
+                cout << -1 << "\n";
+            else {
+                cout << q.front() << "\n";
+            }
+        }
+        else if (str == "back") {
+            if (q.empty())
+                cout << -1 << "\n";
+            else {
+                cout << q.back() << "\n";
+            }
+        }
+    }
+}
