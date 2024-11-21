0893cc33dcd23e75b224d9a82ed0fe3fdd1a1e0d
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-11-22 01:15:56 +0900

    1904

---
+++ Project1/1904.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long dp[1000001];
+
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++)
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+
+    cout << dp[n];
+}
