d688659f4a4bb7d8a7fb9a5e03175802dd4574c5
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2024-12-11 23:43:41 +0900

    2741

---
+++ Project1/2741.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout << i << "\n";
+	}
+}
